e019feb3540eca88d5118feb39a09bb087b7a4d6
Rekaterina <rkatherine0602@gmail.com>
2019-10-07 21:57:40 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,32 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let arr = expr.split('');
+    let size = 10;
+    let result = []; 
+    
+ for (let i = 0; i <Math.ceil(arr.length/size); i++) {
+    result[i] = arr.slice((i*size), (i*size) + size);
+ }
+    
+ result = result.map(i => i.reduce((a,b) => (a+b)));
+     
+ result = result.map(i => i.replace(/11/g, '-')).map(i => i.replace(/10/g, '.')).map(i => i.replace(/0/g, '')).map(i => i.replace(/\*/g, ' '));
+ 
+ result = result.map((i) => {               
+    for (key in MORSE_TABLE) {
+        if (i === key) {
+            return MORSE_TABLE[key];
+        } 
+        if (i === " ") {
+            return " ";
+        }
+    }
+});
+
+console.log(result);
+result = result.join('');
+return result;
 }
 
 module.exports = {
